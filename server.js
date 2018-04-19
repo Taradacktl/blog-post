a1a53c9c8a778e45c96a1c758c869abd327e4168
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-04-19 10:59:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,9 @@ const blogPostRouter = require('./blogPostRouter');
 
 app.use(morgan('common'));
 
-app.use(express.static('public'));
 
 app.use('/blog-posts', blogPostRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});  
 
 // both runServer and closeServer need to access the same
 // server object, so we declare `server` here, and then when
