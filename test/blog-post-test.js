a5dad60614cac70012f707ef0f330a8b4c272345
Taradacktl <35745484+Taradacktl@users.noreply.github.com>
2018-04-19 11:52:23 -0700

    Update blog-post-test.js

--- test/blog-post-test.js
+++ test/blog-post-test.js
@@ -101,36 +101,56 @@ describe('blog-posts', function() {
   //  5. Inspect the response object to ensure it
   //  has right status code and that we get back an updated
   //  item with the right data in it.
-  it('should update items on PUT', function() {
+  
+    it('should update blog posts on PUT', function() {
+
+    return chai.request(app)
+      // first have to get
+      .get('/blog-posts')
+      .then(function( res) {
+        const updateData = Object.assign(res.body[0], {
+          title: 'weekend',
+          content: 'plans'
+        });
+        return chai.request(app)
+          .put(`/blog-posts/${res.body[0].id}`)
+          .send(updateData)
+          .then(function(res) {
+            expect(res).to.have.status(204);
+          });
+      });
+  });
+  
+//  it('should update items on PUT', function() {
     // we initialize our updateData here and then after the initial
     // request to the app, we update it with an `id` property so
     // we can make a second, PUT call to the app.
-    const updateData = {
-      title: 'weekend',
-      content: 'plans',
-      publishDate: 'march'
-    };
+ //   const updateData = {
+//      title: 'weekend',
+ //     content: 'plans',
+ //     publishDate: 'march'
+//    };
 
-    return chai.request(app)
+  //  return chai.request(app)
       // first have to get so we have an idea of object to update
-      .get('/blog-posts')
-      .then(function(res) {
-        updateData.id = res.body[0].id;
+  //    .get('/blog-posts')
+   //   .then(function(res) {
+    //    updateData.id = res.body[0].id;
         // this will return a promise whose value will be the response
         // object, which we can inspect in the next `then` block. Note
         // that we could have used a nested callback here instead of
         // returning a promise and chaining with `then`, but we find
         // this approach cleaner and easier to read and reason about.
-        return chai.request(app)
-          .put(`/blog-posts/${updateData.id}`)
-          .send(updateData);
+   //     return chai.request(app)
+  //        .put(`/blog-posts/${updateData.id}`)
+  //        .send(updateData);
       })
       // prove that the PUT request has right status code
       // and returns updated item
-      .then(function(res) {
-        expect(res).to.have.status(204);
-      });
-  });
+     // .then(function(res) {
+    //    expect(res).to.have.status(204);
+//      });
+//  });
 
   // test strategy:
   //  1. GET blog-post items so we can get ID of one
